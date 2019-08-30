48eb3d6ddea9e3f29088f59a8b9109ca532a9acd
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-08-30 13:38:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ server = app.server
 
 app.layout = html.Div(
     [
-        html.H1("Demo: Plotly Express in Dash with Tips Dataset"),
-        html.Div(,
-           # [
-            #    html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
-            #    for d in dimensions
-            #],
-            style={"width": "25%", "float": "left"},
-        ),
-        dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"}),
+        html.H1("Plotly Express in Dash with Tips Dataset"),
+        #html.Div(
+        #    [
+        #       html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
+        #        for d in dimensions
+        #    ],
+        #    style={"width": "25%", "float": "left"},
+        #),
+        #dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"}),
     ]
 )
 
