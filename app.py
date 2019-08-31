9a3f7a3c8f2df6568b9747038c0f40fff77a5c22
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-08-31 22:38:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ app = dash.Dash(
 )
 server = app.server
 
-app.layout = html.Div([html.H1("Plotly Express in Dash with Tips Dataset"),])
+app.layout = html.Div([html.H1("Plotly Express in Dash with Tips Dataset"),
+   html.Div([
    fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
           color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
-        fig.show()
+        fig.show()])
+        dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"})
+                      ])        
 #print("Hello World")
 #app.layout = html.Div(
  #   [
