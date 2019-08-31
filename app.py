5b4f119707369d9fc99db0160f1967cde40354fb
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-08-31 22:12:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-#tips = px.data.tips()
+tips = px.data.tips()
 dataSet=pd.read_csv('X&Y.csv')
 #col_options = [dict(label=x, value=x) for x in tips.columns]
 dimensions = ["x", "y", "color", "facet_col", "facet_row"]
@@ -17,7 +17,10 @@ server = app.server
 app.layout = html.Div(
     [
         html.H1("Plotly Express in Dash with Tips Dataset"),
-        fig=px.scatter(dataSet)
+        
+        fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
+           color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
+        fig.show()
         #html.Div(
                      #[
              #  html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
