01f13cded6ef1c9ab0b109d08e516c06d716fbe3
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-08-31 21:58:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-tips = px.data.tips()
-col_options = [dict(label=x, value=x) for x in tips.columns]
+#tips = px.data.tips()
+dataSet=pd.read_csv('X&Y.csv')
+#col_options = [dict(label=x, value=x) for x in tips.columns]
 dimensions = ["x", "y", "color", "facet_col", "facet_row"]
 
 app = dash.Dash(
@@ -17,26 +18,23 @@ app.layout = html.Div(
     [
         html.H1("Plotly Express in Dash with Tips Dataset"),
         html.Div(
-            [
-               html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
-                for d in dimensions
-            ],
-            style={"width": "25%", "float": "left"},
-        ),
+            #[
+             #  html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
+              #  for d in dimensions
+            #],
+            #style={"width": "25%", "float": "left"},
+        #),
         dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"}),
     ]
 )
 
 
-@app.callback(Output("graph", "figure"), [Input(d, "value") for d in dimensions])
-def make_figure(x, y, color, facet_col, facet_row):
+@app.callback(Output("graph", "figure"))
+def make_figure(x, y):
     return px.scatter(
-        tips,
+        dataSet,
         x=x,
         y=y,
-        color=color,
-        facet_col=facet_col,
-        facet_row=facet_row,
         height=700,
     )
 
