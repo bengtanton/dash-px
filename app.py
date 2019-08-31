f387a62e01b3eb3759549ce4d03a13c750278bda
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-08-31 22:21:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
 tips = px.data.tips()
-dataSet=pd.read_csv('X&Y.csv')
+#dataSet=pd.read_csv('X&Y.csv')
 #col_options = [dict(label=x, value=x) for x in tips.columns]
 dimensions = ["x", "y", "color", "facet_col", "facet_row"]
 
@@ -13,9 +13,9 @@ app = dash.Dash(
     __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 )
 server = app.server
-   fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
-          color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
-        fig.show()
+ #  fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
+  #        color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
+   #     fig.show()
 
 #app.layout = html.Div(
  #   [
