dd9010f7e93a193dddee7fe53bf6975f8dae9ec9
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-09-03 22:26:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 df = pd.read_csv(
-    'x&Y.csv')
+     'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
 
 
 def generate_table(dataframe, max_rows=10):
