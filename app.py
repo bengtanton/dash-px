95a7775b0fce8e828c28541d8d9b70a626b0b35d
bengtanton <51060609+bengtanton@users.noreply.github.com>
2019-09-03 21:50:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,48 +7,31 @@ from dash.dependencies import Input, Output
 tips = px.data.tips()
 #dataSet=pd.read_csv('X&Y.csv')
 #col_options = [dict(label=x, value=x) for x in tips.columns]
-dimensions = ["x", "y", "color", "facet_col", "facet_row"]
+#dimensions = ["x", "y", "color", "facet_col", "facet_row"]
 
 app = dash.Dash(
     __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 )
 server = app.server
 
-app.layout = html.Div([html.H1("Plotly Express in Dash with Tips Dataset"),])
-  # html.Div([
-  # fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
-   #       color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
-    #    fig.show()])
-     #   dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"})
-      #                ])        
-#print("Hello World")
-#app.layout = html.Div(
- #   [
-  #      html.H1("Plotly Express in Dash with Tips Dataset"),
-        
-       # fig = px.scatter(tips, x="total_bill", y="tip", color="size", facet_col="sex",
-        #   color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
-        #fig.show()
-        #html.Div(
-                     #[
-             #  html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
-              #  for d in dimensions
-            #],
-            #style={"width": "25%", "float": "left"},
-        #),
-        #dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"}),
-   # ]
-#)
+app.layout = html.Div([html.H1("Plotly Express in Dash with Tips Dataset"),
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
 
-
-#@app.callback(Output("graph", "figure"))
-#def make_figure(x, y):
- #   return px.scatter(
-  #      dataSet,
-   #     x=x,
-    #    y=y,
-     #   height=700,
-    #)
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
